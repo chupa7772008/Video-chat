a93e4e79d5e1b9a8ec65dab7e4cf8906147037c2
Chupa Style <chupa7772008@users.noreply.github.com>
2026-09-13 23:13:22 +0500

    Исправлено подключение PostgreSQL для Render

--- db.js
+++ db.js
@@ -1,11 +1,20 @@
 const { Pool } = require("pg");
 
-const pool = new Pool({
-    host: "localhost",
-    port: 5432,
-    database: "videochat",
-    user: "u0_a849",
-    password: "",
-});
+const pool = new Pool(
+    process.env.DATABASE_URL
+        ? {
+            connectionString: process.env.DATABASE_URL,
+            ssl: {
+                rejectUnauthorized: false
+            }
+        }
+        : {
+            host: "localhost",
+            port: 5432,
+            database: "videochat",
+            user: "u0_a849",
+            password: ""
+        }
+);
 
 module.exports = pool;
